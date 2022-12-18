20c94508a7893af06a10d144d03f95c50792f90e
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2022-12-18 16:41:21 -0600

    making timer count down from 60sec to 0

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 var startQuizButton = document.getElementById("start-quiz")
 var questionScreen = document.getElementById("question-screen")
-var timer = document.getElementById("timer")
+var countDown = document.getElementById("timer");
+var timer;
 
 startQuizButton.addEventListener("click", startQuiz)
+startQuizButton.addEventListener("click", setTime) 
 
 function startQuiz() {
     console.log("it works");
     
+}
+
+function setTime() {
+  var sec = 60;
+  timer = setInterval(() =>{
+    countDown.innerHTML = "00: "+sec;
+    sec --;
+  }, 1000)
 }
 
