0cee6ae84d9bf7dc531c2c522dab5eaa251ef721
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2022-12-18 15:25:03 -0600

    working on startquiz button to start timer countdown

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
 var startQuizButton = document.getElementById("start-quiz")
 var questionScreen = document.getElementById("question-screen")
+var timer = document.getElementById("timer")
 
 startQuizButton.addEventListener("click", startQuiz)
 
 function startQuiz() {
     console.log("it works");
-    startQuizButton.classList.add("hide")
-    questionScreen.classList.remove("hide")
-
+    
 }
 
-function nextQuestion() {
-
-}
-
-function selectAnswer() {
-
-}
