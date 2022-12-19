351243c480d2bcf67687bf6e707c6cf08809ff31
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2022-12-18 20:46:50 -0600

    finished first question and logic for first question

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 var startQuizButton = document.getElementById("app-intro")
+var nextButton = document.getElementById("next-btn")
 var questionScreen = document.getElementById("question-container")
 var countDown = document.getElementById("timer");
 var timer;
+var quizQuestions = document.getElementById("question")
+var answerButtons = document.getElementById("truefalse-buttons")
+var shuffledQuestions, currentQuestionIndex
+
 
 startQuizButton.addEventListener("click", startQuiz)
 startQuizButton.addEventListener("click", setTime) 
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    nextQuestion()
+})
 
 function startQuiz() {
     console.log("it works");
     startQuizButton.classList.add("hide")
     questionScreen.classList.remove("hide")
+    shuffledQuestions = question.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     nextQuestion()
     
     
@@ -24,8 +35,49 @@ function setTime() {
 }
 
 function nextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 
 }
+
+function showQuestion(question) {
+    quizQuestions.innerText = question.question
+    question.answer.array.forEach(answer => {
+        var button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+    });
+
+}
+
+function selectAnswer(e) {
+    var selectedButton = e.target
+    var correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    nextButton.classList.remove("hide")
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct")
+    element.classList.remove("correct")
+}
+
 
 var question = [
     {
